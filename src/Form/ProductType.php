c44858606250c692fefe5f0f31428497473a61b8
medjalil <med.abdeljalil@gmail.com>
2020-09-02 17:40:26 +0100

    update form Type

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,25 +4,25 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
-class ProductType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+class ProductType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('name')
-            ->add('imageFile',FileType::class)
-            //->add('updatedAt')
+                ->add('name')
+                ->add('imageFile', VichImageType::class)
+        //->add('updatedAt')
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults([
             'data_class' => Product::class,
         ]);
     }
+
 }
